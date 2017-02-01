8d4496945c843a9cb498226afd96c7057039bbdb
Urtzi Odriozola <uodriozola@codesyntax.com>
2017-02-01 16:32:47 +0100

    Code refactored

--- scripts/create_torrent.py
+++ scripts/create_torrent.py
@@ -6,21 +6,46 @@ import pdb
 import time
 
 # FILM PATHS
-FILM_PATH_TO_SHARE = ''
-FILM_PATH_MEDIA_STORE = ''
+FILM_PATH_TO_SHARE = ""
+FILM_PATH_MEDIA_STORE = ""
 
 # SERIE PATHS
-SERIES_PATH_TO_SHARE = ''
-SERIES_PATH_MEDIA_STORE = ''
+SERIES_PATH_TO_SHARE = ""
+SERIES_PATH_MEDIA_STORE = ""
 
 # CONFIGURATION
-TORRENT_STORE_PATH = ''
+TORRENT_STORE_PATH = ""
 TRANSMISSION_AUTH = ""
 COMMENT = "TORRETXEAK eskainitako euskarazko media"
 
-def main():
-    # START CREATING FILM TORRENTS
-    files = glob.glob(FILM_PATH_TO_SHARE + '*')
+
+def create_media_store_folders(media_store_path):
+    command = delegator.run("mkdir -p {0}".format(media_store_path), block=True)
+    print command.out
+
+
+def move_file_to_media_store(f, media_store_path)
+    command = delegator.run('mv {0} {1}'.format(f.name, media_store_path), block=True)
+    print command.out
+
+
+def create_torrent_file(file_name, file_name_ext, media_store_path):
+    media_store_file_path = os.path.join(media_store_path, file_name_ext)
+    torrent_name = "%s.torrent" % file_name
+    torrent_file_path = os.path.join(TORRENT_STORE_PATH, torrent_name)
+    command = delegator.run('transmission-create -o {0} -c {1} -t udp://tracker.openbittorrent.com:80 -t udp://open.demonii.com:1337 -t udp://tracker.coppersurfer.tk:6969 -t udp://tracker.leechers-paradise.org:6969 {2}'.format(torrent_file_path, COMMENT, media_store_file_path), block=True) #TODO: Trackerrak aparteko fitxategi edo datu iturri batetik (zerrenda batetik) hartzea komeni da
+    print command.out
+    return torrent_file_path
+
+
+def share_torrent_file(torrent_file_path)
+    command = delegator.run('transmission-remote --auth={0} --add {1}'.format(TRANSMISSION_AUTH, torrent_file_path), block=True)
+    print command.out
+
+
+def process_files(process_path):
+    # START READING TO SHARE FILES
+    files = glob.glob(process_path + '*')
     for name in files:
         try:
             with open(name) as f:
@@ -37,25 +62,23 @@ def main():
                 media_store_path = os.path.join(FILM_PATH_MEDIA_STORE, media_relative_path)
                 
                 # CREATE MEDIA STORE FOLDERS
-                MKDIR_MEDIA_STORE = delegator.run("mkdir -p {0}".format(media_store_path), block=True)
-                # process = subprocess.Popen(MKDIR_MEDIA_STORE.split(), stdout=subprocess.PIPE)
-                
+                create_media_store_folders(media_store_path)
+
                 # MOVE MEDIA TO STORE FOLDER
-                MV_MEDIA_STORE = delegator.run('mv {0} {1}'.format(f.name, media_store_path), block=True)
-                # process = subprocess.Popen(MV_MEDIA_STORE.split(), stdout=subprocess.PIPE)
+                move_file_to_media_store(f, media_store_path)
 
                 # CREATE TORRENT FILE
                 file_name_ext = "%s.%s" % (file_name, file_ext)
-                media_store_file_path = os.path.join(media_store_path, file_name_ext)
-                torrent_file_path = os.path.join(TORRENT_STORE_PATH, file_name_ext)
-                CREATE_TORRENT_CMD = delegator.run('transmission-create -o {0}.torrent -c {1} -t udp://tracker.openbittorrent.com:80 -t udp://open.demonii.com:1337 -t udp://tracker.coppersurfer.tk:6969 -t udp://tracker.leechers-paradise.org:6969 {2}'.format(torrent_file_path, COMMENT, media_store_file_path), block=True) #TODO: Trackerrak aparteko fitxategi edo datu iturri batetik (zerrenda batetik) hartzea komeni da
-                # process = subprocess.Popen(CREATE_TORRENT_CMD.split(), stdout=subprocess.PIPE)
+                torrent_file_path = create_torrent_file(file_name, file_name_ext, media_store_path)
 
                 # SHARE TORRENT
-                SHARE_TORRENT_CMD = delegator.run('transmission-remote --auth={0} --add {1}'.format(TRANSMISSION_AUTH, torrent_file_path), block=True)
-                # process = subprocess.Popen(SHARE_TORRENT_CMD.split(), stdout=subprocess.PIPE)
+                share_torrent_file(torrent_file_path)
+                
         except IOError as exc:
             print "ERROREA: Fitxategiak irakurtzean"
 
+
+
 if __name__ == "__main__": 
-    main()
+    process_files(FILM_PATH_TO_SHARE)
+    process_files(SERIES_PATH_TO_SHARE)
